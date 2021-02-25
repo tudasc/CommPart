d34b22d834ed1efe7639669bdf193269ebd726ce
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2021-02-25 12:42:12 +0100

    fixed run script for cpp input

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ $MPICC -cc=clang -O2 -fopenmp -Xclang -load -Xclang build/mpi_communication_part
 #$MPICC -cc=clang -O2 -fopenmp -Xclang -load -Xclang build/mpi_communication_partition/libmpi_communication_partition.so  -ftime-report $1
 #$MPICC -cc=clang -O2 -fopenmp $1
 elif [ ${1: -4} == ".cpp" ]; then
-$MPICXX -cxx=clang++ -O2  -fopenmp -Xclang -load -Xclang build/mpi_communication_partition/libmpi_assertion_checker.so  $1
+$MPICXX -cxx=clang++ -O2  -fopenmp -Xclang -load -Xclang build/mpi_communication_partition/libmpi_communication_partition.so  $1
 else
 echo "Unknown file suffix, use this script with .c or .cpp files"
 fi
