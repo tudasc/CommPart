b2724ef505e93553df97a339836fa17e12a7dc4b
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2021-03-02 10:39:20 +0100

    added some code from MACH

--- mpi_communication_partition/sending_partitioning.cpp
+++ mpi_communication_partition/sending_partitioning.cpp
@@ -29,6 +29,171 @@
 
 using namespace llvm;
 
+bool is_waitall_matching(ConstantInt *begin_index, ConstantInt *match_index,
+                         CallBase *call) {
+  assert(begin_index->getType() == match_index->getType());
+  assert(call->getCalledFunction() == mpi_func->mpi_waitall);
+  assert(call->getNumArgOperands() == 3);
+
+  Debug(call->dump(); errs() << "Is this waitall matching?";);
+
+  if (auto *count = dyn_cast<ConstantInt>(call->getArgOperand(0))) {
+
+    auto begin = begin_index->getSExtValue();
+    auto match = match_index->getSExtValue();
+    auto num_req = count->getSExtValue();
+
+    if (begin + num_req > match && match >= begin) {
+      // proven, that this request is part of the requests waited for by the
+      // call
+      Debug(errs() << "  TRUE\n";);
+      return true;
+    }
+  }
+
+  // could not prove true
+  Debug(errs() << "  FALSE\n";);
+  return false;
+}
+
+std::vector<CallBase *> get_matching_waitall(AllocaInst *request_array,
+                                             ConstantInt *index) {
+  std::vector<CallBase *> result;
+
+  for (auto u : request_array->users()) {
+    if (auto *call = dyn_cast<CallBase>(u)) {
+      if (call->getCalledFunction() == mpi_func->mpi_wait && index->isZero()) {
+        // may use wait like this
+        result.push_back(call);
+      }
+      if (call->getCalledFunction() == mpi_func->mpi_waitall) {
+        if (is_waitall_matching(ConstantInt::get(index->getType(), 0), index,
+                                call)) {
+          result.push_back(call);
+        }
+      }
+    } else if (auto *gep = dyn_cast<GetElementPtrInst>(u)) {
+      if (gep->getNumIndices() == 2 && gep->hasAllConstantIndices()) {
+        auto *index_it = gep->idx_begin();
+        ConstantInt *i0 = dyn_cast<ConstantInt>(&*index_it);
+        index_it++;
+        ConstantInt *index_in_array = dyn_cast<ConstantInt>(&*index_it);
+        if (i0->isZero()) {
+
+          for (auto u2 : gep->users()) {
+            if (auto *call = dyn_cast<CallBase>(u2)) {
+              if (call->getCalledFunction() == mpi_func->mpi_wait &&
+                  index == index_in_array) {
+                // may use wait like this
+                result.push_back(call);
+              }
+              if (call->getCalledFunction() == mpi_func->mpi_waitall) {
+                if (is_waitall_matching(index_in_array, index, call)) {
+                  result.push_back(call);
+                }
+              }
+            }
+          }
+
+        } // end if index0 == 0
+      }   // end if gep has simple structure
+    }
+  }
+
+  return result;
+}
+
+std::vector<CallBase *> get_corresponding_wait(CallBase *call) {
+
+  // errs() << "Analyzing scope of \n";
+  // call->dump();
+
+  std::vector<CallBase *> result;
+  unsigned int req_arg_pos = 6;
+  if (call->getCalledFunction() == mpi_func->mpi_Ibarrier) {
+    assert(call->getNumArgOperands() == 2);
+    req_arg_pos = 1;
+  } else {
+    assert(call->getCalledFunction() == mpi_func->mpi_Isend ||
+           call->getCalledFunction() == mpi_func->mpi_Ibsend ||
+           call->getCalledFunction() == mpi_func->mpi_Issend ||
+           call->getCalledFunction() == mpi_func->mpi_Irsend ||
+           call->getCalledFunction() == mpi_func->mpi_Irecv ||
+           call->getCalledFunction() == mpi_func->mpi_Iallreduce);
+    assert(call->getNumArgOperands() == 7);
+  }
+
+  Value *req = call->getArgOperand(req_arg_pos);
+
+  // req->dump();
+  if (auto *alloc = dyn_cast<AllocaInst>(req)) {
+    for (auto *user : alloc->users()) {
+      if (auto *other_call = dyn_cast<CallBase>(user)) {
+        if (other_call->getCalledFunction() == mpi_func->mpi_wait) {
+          assert(other_call->getNumArgOperands() == 2);
+          assert(other_call->getArgOperand(0) == req &&
+                 "First arg of MPi wait is MPI_Request");
+          // found end of scope
+          // errs() << "possible ending of scope here \n";
+          // other_call->dump();
+          result.push_back(other_call);
+        }
+      }
+    }
+  }
+  // scope detection in basic waitall
+  // ofc at some point of pointer arithmetic, we cannot follow it
+  else if (auto *gep = dyn_cast<GetElementPtrInst>(req)) {
+    if (gep->isInBounds()) {
+      if (auto *req_array = dyn_cast<AllocaInst>(gep->getPointerOperand())) {
+        if (gep->getNumIndices() == 2 && gep->hasAllConstantIndices()) {
+
+          auto *index_it = gep->idx_begin();
+          ConstantInt *i0 = dyn_cast<ConstantInt>(&*index_it);
+          index_it++;
+          ConstantInt *index_in_array = dyn_cast<ConstantInt>(&*index_it);
+          if (i0->isZero()) {
+
+            auto temp = get_matching_waitall(req_array, index_in_array);
+            result.insert(result.end(), temp.begin(), temp.end());
+
+          } // end it index0 == 0
+        }   // end if gep has a simple structure
+        else {
+          // debug
+          Debug(gep->dump();
+                errs()
+                << "This structure is currently too complicated to analyze";);
+        }
+      }
+
+    } else { // end if inbounds
+      gep->dump();
+      errs() << "Strange, out of bounds getelemptr instruction should not "
+                "happen in this case\n";
+    }
+  }
+
+  if (result.empty()) {
+    errs() << "could not determine scope of \n";
+    call->dump();
+    errs() << "Assuming it will finish at mpi_finalize.\n"
+           << "The Analysis result is still valid, although the chance of "
+              "false positives is higher\n";
+  }
+
+  // mpi finalize will end all communication nontheles
+  for (auto *user : mpi_func->mpi_finalize->users()) {
+    if (auto *finalize_call = dyn_cast<CallBase>(user)) {
+      assert(finalize_call->getCalledFunction() == mpi_func->mpi_finalize);
+      result.push_back(finalize_call);
+    }
+  }
+
+  return result;
+}
+
+
 inline bool should_be_excluded(User *u,
 		const std::vector<Instruction*> exclude_instructions) {
 
@@ -78,6 +243,11 @@ bool should_function_call_be_considered_modification(Value *ptr,
 		if (buffer_ptr->hasNoCaptureAttr()
 				&& buffer_ptr->hasAttribute(Attribute::ReadOnly)) {
 			// readonly and nocapture: nothing need to be done
+
+			//TODO actually it is allowed to load from ptr and then use a GEP to load it
+			// in some cases a firstprivate message should be used!!
+			errs() << "buffer has readonly\n";
+
 			return false;
 		} else {
 			return true;
@@ -211,7 +381,6 @@ Instruction* search_for_pointer_modification(Value *ptr,
 				// add all uses of this stores Pointer operand "taint" this pointer and treat it as the ptr itself
 				// this will lead to false positives
 				//but there are not much meaningful uses of this anyway
-
 				modification_points.push_back(
 						get_latest_modification_of_pointer(
 								store->getPointerOperand(), search_before,
@@ -223,7 +392,7 @@ Instruction* search_for_pointer_modification(Value *ptr,
 
 		} else if (auto *call = dyn_cast<CallInst>(current_instruction)) {
 			if (should_function_call_be_considered_modification(ptr, call)) {
-				return call;
+				modification_points.push_back(call);
 			}
 		} else if (auto *load = dyn_cast<LoadInst>(current_instruction)) {
 			assert(load->getPointerOperand() == ptr);
